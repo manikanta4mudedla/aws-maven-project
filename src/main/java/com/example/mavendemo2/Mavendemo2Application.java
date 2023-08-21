4aacf855346f3ef977f6e410a9abe30e10fc7182
root <root@ip-172-31-81-76.ec2.internal>
2023-08-21 13:38:07 +0000

    firstcode

---
+++ src/main/java/com/example/mavendemo2/Mavendemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.mavendemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mavendemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mavendemo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mavendemo2/Mavendemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mavendemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mavendemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
